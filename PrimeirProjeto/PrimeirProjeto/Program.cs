567f6aba3aa616797af5cfa659d59185e36b7581
RNalin29 <164230343+RNalin29@users.noreply.github.com>
2024-03-20 15:21:29 -0300

    Primeiro projeto

---
+++ PrimeirProjeto/PrimeirProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+
+double largura, comprimento, precoMetroQuadrado, area, preco;
+
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+area = largura * comprimento;
+preco = area * precoMetroQuadrado;
+
+Console.WriteLine("Area = " + area.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Preco = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+Console.ReadLine();
---
+++ PrimeirProjeto/Projeto-iniciante1.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+
+int n1 = int.Parse(Console.ReadLine());
+char ch = char.Parse(Console.ReadLine());
+double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //pode usar (.) em fez de somente (,) obs: ñ pode usar virgular 
+
+string[] vet = Console.ReadLine().Split(' '); //guarda as informçãos digitadas em ordem de digitação
+string nome = vet[0];
+char sexo = char.Parse(vet[1]);
+int idade = int.Parse(vet[2]);
+double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Você digitou: ");
+Console.WriteLine(n1);
+Console.WriteLine(ch);
+Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture)); // F2 cada decimal. em vez de aparecer (,) vai aparecer (.) 
+Console.WriteLine(nome);
+Console.WriteLine(sexo);
+Console.WriteLine(idade);
+Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
